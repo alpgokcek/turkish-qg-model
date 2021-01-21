f1114a8b0663801256e7fb21c87eef69e0999f07
alpgokcek <alp.gokcek1@gmail.com>
2021-01-21 21:39:45 +0300

    update

--- Preliminary_QG/Main.py
+++ Preliminary_QG/Main.py
@@ -3,7 +3,7 @@ from fuzzywuzzy import fuzz
 from PersonDataParser import Parser
 from QuestionPatterns import QuestionPatterns
 from bs4 import BeautifulSoup
-import time
+import json
 THRESHOLD = 70
 parser = Parser("everyone.txt")
 parser.parse()
@@ -14,23 +14,7 @@ patterns = QuestionPatterns.patterns
 out = open("out_data.txt", 'w')
 full_wiki = open("wiki_new1.html").read()
 soup = BeautifulSoup(full_wiki, "html.parser")
-start=time.time()
-long_info = soup.find("div", {"id": 16}).get_text()
-end=time.time()
-print(end-start)
-start=time.time()
-long_info = soup.find("div", {"id": 10}).get_text()
-end=time.time()
-print(end-start)
-start=time.time()
-long_info = soup.find("div", {"id": 1442}).get_text()
-end=time.time()
-print(end-start)
-start=time.time()
-long_info = soup.find("div", {"id": 3886}).get_text()
-end=time.time()
-print(end-start)
-print(long_info, type(long_info))
+
 '''
 {
     data: [
@@ -58,26 +42,27 @@ def main():
         print(i)
         i+=1
         temp_dict = dict()
-        description = soup.find("div", {"id": int(p.doc_id)})
-        temp_dict['description'] = description
-        temp_dict['data'] = list()
-        for pattern_type in patterns[p.occupation].keys():
-            if pattern_type in p.attributes.keys():
-                answer = p.attributes[pattern_type]
-                temp_dict1 = {'answer': answer, 'questions': list()}
-                for question in patterns[p.occupation][pattern_type]:                
-                    if '~' not in question and '#' not in question: 
-                        ratio = fuzz.partial_ratio(description, p.attributes[pattern_type])
-                        if ratio > THRESHOLD:
-                            temp_dict1['questions'].append(question.format(name=p.name))
-                if len(temp_dict1['questions']) > 0:
-                    temp_dict['data'].append(temp_dict1)
-        output_dict['data'].append(temp_dict)
-print(output_dict)
+        description_tag = soup.find("div", {"id": int(p.doc_id)})
+        if description_tag:
+            description = description_tag.get_text()
+            temp_dict['description'] = description
+            temp_dict['data'] = list()
+            for pattern_type in patterns[p.occupation].keys():
+                if pattern_type in p.attributes.keys():
+                    answer = p.attributes[pattern_type]
+                    temp_dict1 = {'answer': answer, 'questions': list()}
+                    for question in patterns[p.occupation][pattern_type]:                
+                        if '~' not in question and '#' not in question: 
+                            ratio = fuzz.partial_ratio(description, p.attributes[pattern_type])
+                            if ratio > THRESHOLD:
+                                temp_dict1['questions'].append(question.format(name=p.name))
+                    if len(temp_dict1['questions']) > 0:
+                        temp_dict['data'].append(temp_dict1)
+            out.write(json.dumps(temp_dict))
 
 
 
 
 if __name__ == '__main__':
-    #main()
+    main()
     pass
