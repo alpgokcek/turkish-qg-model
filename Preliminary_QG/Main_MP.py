2217f2b5d2417e85a6fe2b4dbffeb5948a5f5d63
Erdal Dogan <erdalsidal99@gmail.com>
2021-03-27 18:03:08 +0300

    Extract common questions to a function

--- Preliminary_QG/Main_MP.py
+++ Preliminary_QG/Main_MP.py
@@ -13,16 +13,16 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--output", help="path of output file",
-                    type=str)
+					type=str)
 parser.add_argument(
-    "--dev", help="development environment selection", action='store_true')
+	"--dev", help="development environment selection", action='store_true')
 args = parser.parse_args()
 
 ATTRIBUTES_PATH = "sample_data/sample_wiki_persons.txt" if args.dev else "data/wiki_persons.txt"
 WIKI_PATH = "sample_data/sample_wiki_dump.html" if args.dev else "data/wiki_whole_data.html"
 OUTPUT_PATH = "out/data.txt" if not args.output else args.output
 
-THRESHOLD = 70
+THRESHOLD = 60
 parser = Parser(ATTRIBUTES_PATH)
 parser.parse()
 
@@ -47,89 +47,87 @@ _suffix1, _suffix2, _suffix3 = "_suffix1", "_suffix2", "_suffix3"
 
 
 def get_suffix(word: str, sffx_type: str):
-    suffix = ''
-    if sffx_type == "_suffix1":
-        suffix = Turkish(word).genitive(person=2)
-    elif sffx_type == "_suffix2":
-        suffix = Turkish(word).dative()
-    elif sffx_type == "_suffix3":
-        suffix = Turkish(word).ablative()
-    suffix = str(suffix)
-    return suffix[len(word):]
+	suffix = ''
+	if sffx_type == "_suffix1":
+		suffix = Turkish(word).genitive(person=2)
+	elif sffx_type == "_suffix2":
+		suffix = Turkish(word).dative()
+	elif sffx_type == "_suffix3":
+		suffix = Turkish(word).ablative()
+	suffix = str(suffix)
+	return suffix[len(word):]
 
+def format_question(p, question_pattern):
+	try:
+		if _suffix1 in question_pattern:
+			question_pattern = question_pattern.format(
+				name=p.name, _suffix1=get_suffix(p.name, _suffix1))
+		elif _suffix2 in question_pattern:
+			question_pattern = question_pattern.format(
+				name=p.name, _suffix2=get_suffix(p.name, _suffix2))
+		elif _suffix3 in question_pattern:
+			question_pattern = question_pattern.format(
+				name=p.name, _suffix3=get_suffix(p.name, _suffix3))
+		else:
+			question_pattern = question_pattern.format(name=p.name)
+		#qa_pair['questions'].append(question_pattern)
+		return question_pattern
+	except Exception as e:
+		print("Error on: {} - {} \n{}\n".format(e, p.name, question_pattern))
 
-def create_common_questions(p: Person, person_dict: dict):
-	index, p = enum
-	for feature, question_patterns in common_patterns:
-		ans = p.attributes[feature]
-		for pattern in question_patterns:
-			try:
-				if _suffix1 in question_pattern:
-				    question_pattern = question_pattern.format(
-				        name=p.name, _suffix1=get_suffix(p.name, _suffix1))
-				elif _suffix2 in question_pattern:
-				    question_pattern = question_pattern.format(
-				        name=p.name, _suffix2=get_suffix(p.name, _suffix2))
-				elif _suffix3 in question_pattern:
-				    question_pattern = question_pattern.format(
-				        name=p.name, _suffix3=get_suffix(p.name, _suffix3))
-				else:
-				    question_pattern = question_pattern.format(name=p.name)
-				qa_pair['questions'].append(question_pattern)
-			except Exception as e:
-				print("Error on: {} - {} \n{}\n".format(e, p.name, question_pattern))
-
-
+def create_common_questions(p: Person):
+	global counter
+	#index, p = enum
+	pair_list = list()
+	for feature, question_patterns in common_patterns.items():
+		if feature in p.attributes:
+			ans = p.attributes[feature]
+			qa_pair = {'answer': ans, 'questions': list()}
+			for pattern in question_patterns:
+				q = format_question(p, pattern)
+				if q:
+					qa_pair['questions'].append(q)
+			pair_list.append(qa_pair)
+	return pair_list
+			
 
 def process(enum):
-    index, p = enum
-    # print("Idx,", index, "P", p)
-    person_dict = dict()
-    description_tag = soup.find("div", {"id": int(p.doc_id)}) # large description text
-    print("DESC TAG.", description_tag)
-    if description_tag: 
-        description = description_tag.get_text().replace('\n', '')
-        del description_tag
-        person_dict['description'] = "description"
-        person_dict['data'] = list()
-        feature_patterns = occupational_patterns[p.occupation]
-        # pattern type = rutbesi, pozisyon, etc.
-        for pattern_type in feature_patterns.keys():
-            if pattern_type in p.attributes.keys():
-                answer = p.attributes[pattern_type]
-                qa_pair = {'answer': answer, 'questions': list()}
-                for question_pattern in feature_patterns[pattern_type]:
-                    ratio = int(fuzz.partial_token_set_ratio(description, answer)) # does the description paragraph contain answer?
-                    if ratio > 30:
-                        qa_pair['ratio'] = ratio
-                        try:
-                            if _suffix1 in question_pattern:
-                                question_pattern = question_pattern.format(
-                                    name=p.name, _suffix1=get_suffix(p.name, _suffix1))
-                            elif _suffix2 in question_pattern:
-                                question_pattern = question_pattern.format(
-                                    name=p.name, _suffix2=get_suffix(p.name, _suffix2))
-                            elif _suffix3 in question_pattern:
-                                question_pattern = question_pattern.format(
-                                    name=p.name, _suffix3=get_suffix(p.name, _suffix3))
-                            else:
-                                question_pattern = question_pattern.format(name=p.name)
-                            qa_pair['questions'].append(question_pattern)
-                        except Exception as e:
-                            print("Error on: {} - {} \n{}\n".format(e,
-                                                                    p.name, question_pattern))
-                if len(qa_pair['questions']) > 0: # if any relevant qa pair is present
-                	person_dict['data'].append(qa_pair)
-                else:
-                	del qa_pair
-        if len(person_dict['data']) > 0:
-        	out.write(json.dumps(person_dict, ensure_ascii=False)+ ", \n\n")
-        del person_dict
+	index, p = enum
+	# print("Idx,", index, "P", p)
+	person_dict = dict()
+	description_tag = soup.find("div", {"id": int(p.doc_id)}) # large description text
+	if description_tag:
+		description = description_tag.get_text().replace('\n', '')
+		del description_tag
+		person_dict['description'] = "description"
+		person_dict['data'] = list()
+		common_questions = create_common_questions(p)
+		person_dict['data'].append(common_questions)
+		feature_patterns = occupational_patterns[p.occupation]
+		# pattern type = rutbesi, pozisyon, etc.
+		for pattern_type in feature_patterns.keys():
+			if pattern_type in p.attributes.keys():
+				answer = p.attributes[pattern_type]
+				qa_pair = {'answer': answer, 'questions': list()}
+				for question_pattern in feature_patterns[pattern_type]:
+					ratio = int(fuzz.partial_token_set_ratio(description, answer)) # does the description paragraph contain answer?
+					if ratio > THRESHOLD:
+						qa_pair['ratio'] = ratio
+						q = format_question(p, question_pattern)
+						if q:
+					   		qa_pair['questions'].append(q)
+				if len(qa_pair['questions']) > 0: # if any relevant qa pair is present
+					person_dict['data'].append(qa_pair)
+				else:
+					del qa_pair
+		if len(person_dict['data']) > 0:
+			out.write(json.dumps(person_dict, ensure_ascii=False)+ ", \n\n")
+		del person_dict
 
 
 if __name__ == '__main__':
-    pool = Pool(os.cpu_count())  # Create a multiprocessing Pool
-    # pool.map(create_common_questions, enumerate(persons))
-    pool.map(process, enumerate(persons))
-    pool.close()
-    pool.join()
+	pool = Pool(os.cpu_count())  # Create a multiprocessing Pool
+	#pool.map(create_common_questions, enumerate(persons))
+	pool.map(process, enumerate(persons))
+	pool.close()
+	pool.join()
