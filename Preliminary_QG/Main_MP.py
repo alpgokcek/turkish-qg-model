66170160ed43c0e3eb144aacc79b61111ddad844
Erdal Dogan <erdalsidal99@gmail.com>
2021-03-27 15:06:14 +0300

    Seperate common patterns from occupational

--- Preliminary_QG/Main_MP.py
+++ Preliminary_QG/Main_MP.py
@@ -2,6 +2,7 @@ from pprint import pprint
 from fuzzywuzzy import fuzz
 from PersonDataParser import Parser
 from QuestionPatterns import QuestionPatterns
+from CommonPatterns import  CommonPatterns
 from bs4 import BeautifulSoup
 import json
 import os
@@ -9,14 +10,16 @@ from multiprocessing import Pool
 from turkish_suffix_library.turkish import Turkish
 
 THRESHOLD = 70
-parser = Parser("data/wiki_persons.txt")
+parser = Parser("sample_data/sample_wiki_persons.txt")
 parser.parse()
 
 counter = 0
-patterns = QuestionPatterns.patterns
+occupational_patterns = QuestionPatterns.patterns
+common_patterns = CommonPatterns.patterns
+
 
 out = open("out/data.txt", 'w')
-full_wiki = open("data/wiki_whole_data.html").read()
+full_wiki = open("sample_data/sample_wiki_dump.html").read()
 soup = BeautifulSoup(full_wiki, "html.parser")
 
 
@@ -42,21 +45,29 @@ def get_suffix(word: str, sffx_type: str):
     return suffix[len(word):]
 
 
+def create_common_questions(enum):
+	index, p = enum
+	for question in common_patterns:
+		print("Person: ",p)
+		print("Question: ",question)
+		print("\n\n")
+
 def process(enum):
     index, p = enum
     print("Idx,", index, "P", p)
-    temp_dict = dict()
-    description_tag = soup.find("div", {"id": int(p.doc_id)})
-    if description_tag:
+    person_dict = dict()
+    description_tag = soup.find("div", {"id": int(p.doc_id)}) # large description text
+    if description_tag: 
         description = description_tag.get_text().replace('\n', '')
         del description_tag
-        temp_dict['description'] = description
-        temp_dict['data'] = list()
-        for pattern_type in patterns[p.occupation].keys():
+        person_dict['description'] = "description"
+        person_dict['data'] = list()
+        feature_patterns = occupational_patterns[p.occupation]
+        for pattern_type in feature_patterns.keys(): # pattern type = rutbesi, pozisyon, etc.
             if pattern_type in p.attributes.keys():
                 answer = p.attributes[pattern_type]
-                temp_dict1 = {'answer': answer, 'questions': list()}
-                for question in patterns[p.occupation][pattern_type]:
+                qa_pair = {'answer': answer, 'questions': list()}
+                for question in feature_patterns[pattern_type]:
                     temp_question = question
                     #print(temp_question)
                     ratio = fuzz.partial_ratio(
@@ -74,22 +85,22 @@ def process(enum):
                                     name=p.name, _suffix3=get_suffix(p.name, _suffix3))
                             else:
                                 temp_question = question.format(name=p.name)
-                            temp_dict1['questions'].append(temp_question)
+                            qa_pair['questions'].append(temp_question)
                         except Exception as e:
                             print("Error on: {} - {} \n{}\n".format(e,
                                                                     p.name, temp_question))
-                if len(temp_dict1['questions']) > 0:
-                    temp_dict['data'].append(temp_dict1)
+                if len(qa_pair['questions']) > 0:
+                    person_dict['data'].append(qa_pair)
                 else:
-                    del temp_dict1
-        if len(temp_dict['data']) > 0:
-            out.write(json.dumps(temp_dict, ensure_ascii=False) + ", \n")
-        del temp_dict
+                    del qa_pair
+        if len(person_dict['data']) > 0:
+            out.write(json.dumps(person_dict, ensure_ascii=False) + ", \n")
+        del person_dict
 
 
 if __name__ == '__main__':
     pool = Pool(os.cpu_count())  # Create a multiprocessing Pool
-   # pool.map(get_suffix, enumerate(persons))
-    pool.map(process, enumerate(persons))
+    pool.map(create_common_questions, enumerate(persons))
+    #pool.map(process, enumerate(persons))
     pool.close()
     pool.join()
